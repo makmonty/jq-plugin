9e01468fe369917b660952f9180bd6b25ab6c7dc
Jesús Germade <jesus@germade.es>
2015-10-14 00:17:33 +0200

    increasing version

--- test.js
+++
@@ -1,9 +0,0 @@
-(function () {
-	'use strict';
-
-	var css = require('./css.js');
-
-	var obj = css('img#jur-jur-jur.hola.caracola[hola="neng"] div.awesome span')
-
-	console.log( 'css', obj.toHTML() );
-})();
